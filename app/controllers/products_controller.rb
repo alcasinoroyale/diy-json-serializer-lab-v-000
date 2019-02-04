67f54db6975375fc32580a1a626408fbbe9333b9
Alex Zdatny <alexzdatny@gmail.com>
2019-02-04 23:37:18 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,6 +22,8 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+  def show
+  end
   def data
     product = Product.find(params[:id])
     render json: ProductSerializer.serialize(product)
