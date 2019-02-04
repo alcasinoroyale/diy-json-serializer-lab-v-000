2721f30bd3044d72ec4242728a4e0ca1d179820a
Alex Zdatny <alexzdatny@gmail.com>
2019-02-04 23:32:12 +0000

    Automatically backed up by Learn

---
+++ app/serializers/product_serializer.rb
@@ -0,0 +1,10 @@
+class ProductSerializer
+  def self.serialize(product)
+
+  serialized_product = '{'
+
+  serialized_product += '"name": ' + product.name + ', '
+
+  serialized_product = '}'
+
+end
