aab61a1921460f97cc7d183428a85f66204163b8
Alex Zdatny <alexzdatny@gmail.com>
2019-02-04 23:35:23 +0000

    Automatically backed up by Learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -4,9 +4,9 @@ class ProductSerializer
   serialized_product = '{'
 
   serialized_product += '"id": ' + product.id.to_s + ', '
-  serialized_product += '"name": ' + product.name + ', '
-  serialized_product += '"price": ' + product.price + ', '
-  serialized_product += '"inventory": ' + product.inventory + ', '
+  serialized_product += '"name": "' + product.name + '", '
+  serialized_product += '"price": ' + product.price.to_s + ', '
+  serialized_product += '"inventory": ' + product.inventory.to_s + ', '
   serialized_product += '"description": ' + product.name + ', '
 
   serialized_product += '}'
