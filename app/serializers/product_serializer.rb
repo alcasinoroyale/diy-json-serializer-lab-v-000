4c833ea1487a2e71546be5747bd7e15ad237769d
Alex Zdatny <alexzdatny@gmail.com>
2019-02-04 23:33:10 +0000

    Automatically backed up by Learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -4,6 +4,10 @@ class ProductSerializer
   serialized_product = '{'
 
   serialized_product += '"name": ' + product.name + ', '
+  serialized_product += '"price": ' + product.price + ', '
+  serialized_product += '"inventory": ' + product.inventory + ', '
+  serialized_product += '"description": ' + product.name + ', '
+
 
   serialized_product = '}'
 
